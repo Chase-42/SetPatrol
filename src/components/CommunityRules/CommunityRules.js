cba42ccbfbd0bf405406f0cda81434a734342655
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-10-17 13:41:50 -0400

    formatMatchStaticPages

---
+++ src/components/CommunityRules/CommunityRules.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import './CommunityRules.css'
+
+const CommunityRules = () => {
+    return (
+        <div className='container'>
+              <h1>SetPatrol Community Rules</h1>
+              <p>These Community Rules, together with our Terms of Service and Privacy Policy, constitute the agreement between you and SetPatrol. If you do not want to agree to the Community Rules, you must not use SetPatrol.</p>
+              <p>Please take these rules seriously and honor them in the spirit in which they are intended. We will do our best to enforce them consistently and fairly, and ultimately we’ll try to do what we think is best in each situation.</p>
+            <h1>SetPatrol Rentals</h1>
+            <h2>Owners</h2>
+                <ul>
+                    <li>Keep it clean.</li>
+                </ul>
+            <p>Make sure all of your gear is tested, cleaned, and fully functional.</p>
+                <ul>
+                    <li>Commit to the rental.</li>
+                </ul>
+            <p>Only accept requests you intend to proceed with. Your renter is depending on you and your equipment.</p>
+                <ul>
+                    <li>Do the paperwork.</li>
+                </ul>
+            <p>Practice good check-in and check-out procedures. Always use the checksheets.</p>
+                <ul>
+                    <li>Be helpful.</li>
+                </ul>
+            <p>Renters will remember the extra care and effort you put in. Your service is your reputation.</p>
+                <ul>
+                    <li>Keep it on SetPatrol.</li>
+                </ul>
+            <p>Don’t try to take transactions or messaging outside the site. Don’t encourage others to breach our Terms of Use.</p>
+         </div>
+    )
+}
+
+export default CommunityRules
--- src/containers/CommunityRulesPage/CommunityRulesPage.js
+++ src/containers/CommunityRulesPage/CommunityRulesPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StaticPage, TopbarContainer } from '../../containers';
 import {
+  CommunityRules,
   LayoutSingleColumn,
   LayoutWrapperTopbar,
   LayoutWrapperMain,
@@ -27,20 +28,8 @@ const CommunityRulesPage = () => {
           <TopbarContainer />
         </LayoutWrapperTopbar>
         <LayoutWrapperMain>
-          <div className='container'>
-              <h1>SetPatrol Community Rules</h1>
-              <p>These Community Rules, together with our Terms of Service and Privacy Policy, constitute the agreement between you and SetPatrol. If you do not want to agree to the Community Rules, you must not use SetPatrol.</p>
-              <p>Please take these rules seriously and honor them in the spirit in which they are intended. We will do our best to enforce them consistently and fairly, and ultimately we’ll try to do what we think is best in each situation.</p>
-            <h1>SetPatrol Rentals</h1>
-            <h2>Owners</h2>
-                <ul>
-                    <li>Keep it clean.</li>
-                </ul>
-            <p>Make sure all of your gear is tested, cleaned, and fully functional.</p>
-                <ul>
-                    <li>Commit to the rental.</li>
-                </ul>
-            <p>Only accept requests you intend to proceed with. Your renter is depending on you and your equipment.</p>
+          <div>
+              <CommunityRules /> 
           </div>
         </LayoutWrapperMain>
         <LayoutWrapperFooter>
