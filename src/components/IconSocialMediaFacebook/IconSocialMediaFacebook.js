280f9c2272a8baeee868ddfb560838df5328c679
Natasha <natashanodine@gmail.com>
2020-10-19 12:01:01 -0500

    commmit changes to social media

--- src/components/IconSocialMediaFacebook/IconSocialMediaFacebook.js
+++ src/components/IconSocialMediaFacebook/IconSocialMediaFacebook.js
@@ -11,7 +11,7 @@ const IconSocialMediaFacebook = props => {
     <svg
       className={classes}
       width="37"
-      height="37"
+      height="37" 
       viewBox="0 0 30 25"
       xmlns="http://www.w3.org/2000/svg"
     >
