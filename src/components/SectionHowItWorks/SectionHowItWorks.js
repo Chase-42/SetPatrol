28f2bbc7ba40479cb9f8d29547d19d2cc0b68414
Natasha <natashanodine@gmail.com>
2020-10-21 10:04:23 -0500

    css update how it works

--- src/components/SectionHowItWorks/SectionHowItWorks.js
+++ src/components/SectionHowItWorks/SectionHowItWorks.js
@@ -22,7 +22,7 @@ const SectionHowItWorks = props => {
         <div className={css.step}>
           <h2 className={css.stepTitle}>
           <h1 className='center'><i className="fa fa-id-badge" aria-hidden="true"></i></h1>
-            <span id="SectionHowItWorks">1. Fill out your profile</span>
+            <span id="SectionHowItWorks" className={css.stepHeader}>1. Fill out your profile</span>
           </h2>
           <p>
             <span id="SectionHowItWorks.part1Text">Upload a photo and add a short description of yourself, your work, interests, social media, and job title. This will help build trust with prospective renters and give them confidence that you're a real human being! Only your public description and listings will be viewable by other members. Your address, email, and phone number remain confidential to protect your safety.</span>
@@ -32,7 +32,7 @@ const SectionHowItWorks = props => {
         <div id='grid'  className={css.step}>
           <h2 className={css.stepTitle}>
             <h1 className='center'><i className="fa fa-money"></i></h1>
-            <span id="SectionHowItWorks">2. Set up payments to rent out gear you own:</span>
+            <span id="SectionHowItWorks" className={css.stepHeader}>2. Set up payments to rent out gear you own:</span>
           </h2>
           <p>
             <span id="SectionHowItWorks">To accept payments for rentals of gear you own, you must set up your payments profile here. We use Paypal, which is the trusted payment for many businesses. SetPatrol does not see or save your bank information; only PayPal has access to it.</span>
@@ -42,7 +42,7 @@ const SectionHowItWorks = props => {
         <div id='grid'  className={css.step}>
           <h2 className={css.stepTitle}>
           <h1 className='center'><i className="fa fa-camera-retro"></i></h1>
-            <span id="SectionHowItWorks">3. List your gear</span>
+            <span id="SectionHowItWorks" className={css.stepHeader}>3. List your gear</span>
           </h2>
           <p>
             <span id="SectionHowItWorks">Creating new listings takes only a few minutes! Provide details about your equipment, and make sure to add a photo. We recommend setting a price between 2% and 5% of the current value. When someone wants to rent, they'll send you an email or make a request.</span>
@@ -52,7 +52,7 @@ const SectionHowItWorks = props => {
         <div id='grid'  className={css.step}>
           <h2 className={css.stepTitle}>
           <h1 className='center'><i class="fa fa-cog" aria-hidden="true"></i></h1>
-            <span id="SectionHowItWorks">4. Find gear you need</span>
+            <span id="SectionHowItWorks" className={css.stepHeader}>4. Find gear you need</span>
           </h2>
           <p>
             <span id="SectionHowItWorks">Search on SetPatrol database to find what you need. You can search by keyword, category, location, or owner. When you find equipment to rent, choose dates, and send a request to the equipment owner via the "Request Gear" button. When the owner accepts your request, set up a time and place to meet for the equipment pickup.</span>
@@ -62,7 +62,7 @@ const SectionHowItWorks = props => {
         <div id='grid'  className={css.step}>
           <h2 className={css.stepTitle}>
             <h1 className='center'><i class="fa fa-exchange" aria-hidden="true"></i></h1>
-            <span id="SectionHowItWorks">5. Equipment handoff</span>
+            <span id="SectionHowItWorks" className={css.stepHeader}>5. Equipment handoff</span>
           </h2>
           <p>
             <span id="SectionHowItWorks">During the equipment handoff, make sure the renter checks that the equipment is what they expected and is in good working condition. Now go make something beautiful!</span>
@@ -72,7 +72,7 @@ const SectionHowItWorks = props => {
         <div id='grid'  className={css.step}>
           <h2 className={css.stepTitle}>
             <h1 className='center'><i class="fa fa-star" aria-hidden="true"></i></h1>
-            <span id="SectionHowItWorks">6. Review</span>
+            <span id="SectionHowItWorks"  className={css.stepHeader}>6. Review</span>
           </h2>
           <p>
             <span id="SectionHowItWorks">After the rental is complete, pay it forward and don't forget to leave your renter's rating and review. This really helps other users to know where to go.</span>
