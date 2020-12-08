7db91c36f01e56f45811e861757c67e216e410d4
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-12-08 12:32:24 -0500

    language

--- src/app.js
+++ src/app.js
@@ -21,13 +21,8 @@ import configureStore from './store';
 import routeConfiguration from './routeConfiguration';
 import Routes from './Routes';
 import config from './config';
-
-// Flex template application uses English translations as default.
 import defaultMessages from './translations/en.json';
-import defaultMessages from './translations/it.json';
-
-const fallbackLng = ['en'];
-const availableLanguages = ['en', 'it'];
+// Flex template application uses English translations as default.
 
 // If you want to change the language, change the imports to match the wanted locale:
 //   1) Change the language in the config.js file!
--- src/components/TopbarDesktop/TopbarDesktop.js
+++ src/components/TopbarDesktop/TopbarDesktop.js
@@ -18,7 +18,6 @@ import { TopbarSearchForm } from '../../forms';
 
 import css from './TopbarDesktop.css';
 
-
 const TopbarDesktop = props => {
   const {
     className,
@@ -182,6 +181,8 @@ const TopbarDesktop = props => {
     </NamedLink>
   );
 
+  const switchLanguage = () => console.log('ok')
+
   return (
     <nav className={classes}>
       <NamedLink className={css.logoLink} name="LandingPage">
@@ -192,6 +193,7 @@ const TopbarDesktop = props => {
         />
       </NamedLink>
       {search}
+      <button onClick={switchLanguage}>Language</button>
       <NamedLink className={css.createListingLink} name="NewListingPage">
         <span className={css.createListing}>
           <FormattedMessage id="TopbarDesktop.createListing" />
