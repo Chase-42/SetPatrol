10ef918cf7446f33491242ba8d0225a211843468
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-12-22 12:14:21 -0500

    logoUpdatedStillNeedsMoreWOrk

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 import config from '../../config';
-import MobileLogoImage from './setpatrol_logo-small.png';
-import DesktopLogoImage from './setpatrol_logo.png';
+import MobileLogoImage from './SP_logo_square.png';
+import DesktopLogoImage from './long_setpetrol_logo.png';
 import css from './Logo.css';
 
 const Logo = props => {
